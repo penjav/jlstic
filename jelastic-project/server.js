10b8ba28c4146eb4b5683334e8c27e62508932f6
penpagyaltsen <penpagyaltsen@172.31.99.90>
2017-03-12 12:44:50 -0400

    trial

--- jelastic-project/server.js
+++ jelastic-project/server.js
@@ -2,13 +2,17 @@
  * http://usejsdoc.org/
  */
 var express = require("express");
+var path = require("path");
 
 var app = express();
 
 app.set("port",process.env.PORT || 3000);
+app.set("view engine","ejs");
+app.set("views",path.resolve(__dirname,"view"));
+app.use(express.static(path.resolve(__dirname,"view")));
 
 app.get("/",function(req,res){
-	res.send("Hello world! welcome to node. success!");
+	res.render("index");
 }).listen(app.get("port"),function(){
 	console.log("Server started on "+ app.get("port"));
 });
